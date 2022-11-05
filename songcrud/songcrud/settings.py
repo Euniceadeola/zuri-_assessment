6faeaae9f7f60c58845d19b59d063c1ad0c1a2f7
Eunice Adejoke Adeola <euniceadejoke22@gmail.com>
2022-11-05 11:46:39 +0300

    week6 Django RESTFramework

--- songcrud/songcrud/settings.py
+++ songcrud/songcrud/settings.py
@@ -10,35 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-
 from pathlib import Path
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$=w%_p1vwr1bx*)67gn^3$j)ykiep=ws=ytj7n(i=%zzjzcf$s'
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 
 ALLOWED_HOSTS = []
 
 
-
-
 # Application definition
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -48,8 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'musicapp',
+    'rest_framework',
 ]
 
+REST_FRAMEWORK = {
+        "DEFAULT_PERMISION_CLASSES": [
+            "rest_framework.permission.AllowAny"
+        ]
+    }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -61,9 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
 ROOT_URLCONF = 'songcrud.urls'
-
 
 TEMPLATES = [
     {
@@ -81,15 +75,11 @@ TEMPLATES = [
     },
 ]
 
-
 WSGI_APPLICATION = 'songcrud.wsgi.application'
-
-
 
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 
 DATABASES = {
     'default': {
@@ -99,11 +89,8 @@ DATABASES = {
 }
 
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -121,35 +108,24 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-
 LANGUAGE_CODE = 'en-us'
-
 
 TIME_ZONE = 'UTC'
 
-
 USE_I18N = True
 
-
 USE_TZ = True
-
-
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-
 STATIC_URL = 'static/'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
